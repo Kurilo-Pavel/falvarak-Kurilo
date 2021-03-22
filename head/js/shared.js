a11ac096cc07168d5a3593cf04c97a839b41a9db
Pavel <kurilo.pavel@mail.ru>
2021-03-22 16:11:04 +0300

    Changed the file shared.js

--- head/js/shared.js
+++ head/js/shared.js
@@ -1,14 +1,49 @@
+var
+  $smallImage = $(".center_image"),
+  $display = $(".hover_display"),
+  $li = $(".section_ul").find(">li"),
+  $lightBox = $(".double_block"),
+  $close = $(".double_close"),
+  $prev = $(".double_prev"),
+  $next = $(".double_next"),
+  targetImg,
+  liIndex;
 
-
-
-
-$(function () {
-  $(".center_image").click(function (event) {
-    $(".double_image").fadeIn(500);
-    $(".hover_display").addClass("active");
-  });
-  $(".hover_display").click(function () {
-    $(".double_image").fadeOut(500);
-    $(".hover_display").removeClass("active");
-  });
+$smallImage.click(function () {
+  liIndex = $(this).parent().index();
+  targetImg = $(this).attr("src");
+  $lightBox.find("img").attr("src", targetImg);
+  $display.slideDown(1000);
+  $lightBox.fadeIn(1000);
+  $lightBox.css("display", "block");
 });
+
+$close.click(function () {
+  $lightBox.fadeOut(500);
+  $display.slideUp(1000);
+});
+
+$next.click(function () {
+  if ((liIndex + 1) < $li.length) {
+    targetImg = $li.eq(liIndex + 1).find("img").attr("src");
+    liIndex++;
+  } else {
+    targetImg = $li.eq(0).find("img").attr("src");
+    liIndex = 0;
+  }
+  $lightBox.find("img").attr("src", targetImg);
+});
+
+$prev.click(function () {
+  if (liIndex > 0) {
+    targetImg = $li.eq(liIndex - 1).find("img").attr("src");
+    liIndex--;
+  } else {
+    targetImg = $li.eq($li.length - 1).find("img").attr("src");
+    liIndex = $li.length - 1;
+  }
+  $lightBox.find("img").attr("src", targetImg);
+});
+
+
+
